307ca7d1b4b8a9b9eabae3d5bebf4193e7668679
almurafiq <100346794+almurafiq@users.noreply.github.com>
2022-05-12 10:09:18 +0200

    NEW COMMIT

--- lib/layouts/home_layout.dart
+++ lib/layouts/home_layout.dart
@@ -16,92 +16,101 @@ class HomeLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AppCubit, AppStates>(
-      listener: (context, state) {
-        if (state is ConnectionStatusOnlineState)
-         {
+        listener: (context, state) {
+          if (state is ConnectionStatusOnlineState) {
+            showToast(
+                message: 'the internet is online',
+                toastStates: ToastStates.SUCCESS);
+          }
+          if (state is ConnectionStatusOfflineState) {
+            showCupertinoModalPopup(
 
-           showToast(
-             message: 'the internet is online',
-             toastStates: ToastStates.SUCCESS);
-         }
-        if (state is ConnectionStatusOfflineState)
-        {     showCupertinoModalPopup(
+                barrierDismissible: false,
+                builder: (BuildContext context) {
+                  return Center(
+                    child: Container(
+                      color: Colors.white54,
+                      height: MediaQuery
+                          .of(context)
+                          .size
+                          .height * 0.4,
+                      child: Scaffold(
+                        backgroundColor: Colors.white38,
+                        body: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .width * 0.35,
+                                height: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .height * 0.35,
+                                child: Shimmer.fromColors(
+                                  baseColor: Colors.grey,
+                                  highlightColor: defaultColor,
+                                  child: Shimmer.fromColors(
+                                      baseColor: Colors.grey,
+                                      highlightColor: defaultColor,
+                                      child: Image.asset(
+                                          'assets/images/logo_s.png')),
+                                ),
+                              ), Text(
+                                'please check th internet',
+                                style: GoogleFonts.balsamiqSans(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
 
-            barrierDismissible: false,
-            builder: (BuildContext context) {  return    Center(
-                child:Container(
-                  color: Colors.white54,
-                  height:MediaQuery.of(context).size.height * 0.4,
-              child: Scaffold(
-                backgroundColor: Colors.white38,
-                body:         Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.35,
-                      height:MediaQuery.of(context).size.height * 0.35,
-                      child: Shimmer.fromColors(
-                        baseColor: Colors.grey,
-                        highlightColor: defaultColor,
-                        child: Shimmer.fromColors(
-                            baseColor: Colors.grey,
-                            highlightColor: defaultColor,
-                            child: Image.asset(
-                                'assets/images/logo_s.png')),
-                      ),
-                    ),Text(
-                'please check th internet',
-                style: GoogleFonts.balsamiqSans(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-                  ],
-                ),
-              ),
-              ),),
-            )
-    ;
-              }, context: context);
-          showToast(
-              message: 'the internet is offline',
-              toastStates: ToastStates.EROOR);
-        }
-      },
-      builder: (context, state) {
-        var cubit = AppCubit.get(context);
-        // PersistentTabController _controller;
-
-        // _controller = PersistentTabController(initialIndex: 1);
-
-        return  Scaffold(
-        body: cubit.bottomScreens[cubit.currentIndex],
-        bottomNavigationBar: CurvedNavigationBar(
-        height: 55,
-        backgroundColor: defaultColor,
-        index: cubit.currentIndex,
-        // ignore: prefer_const_literals_to_create_immutables
-        items: <Widget>[
-        const Icon(
-        Icons.settings,
-        size: 25,
-        ),
-        const Icon(
-        Icons.home,
-        size: 25,
-        ),
-        const Icon(
-        Icons.person,
-        size: 25,
-        ),
-        ],
-        onTap: (index) {
-        cubit.changeBottomNav(index);
+                            ],
+                          ),
+                        ),
+                      ),),
+                  )
+                  ;
+                }, context: context);
+            showToast(
+                message: 'the internet is offline',
+                toastStates: ToastStates.EROOR);
+          }
         },
-    ),
-    );});
+        builder: (context, state) {
+          var cubit = AppCubit.get(context);
+          // PersistentTabController _controller;
+
+          // _controller = PersistentTabController(initialIndex: 1);
+
+          return Scaffold(
+            body: cubit.bottomScreens[cubit.currentIndex],
+            bottomNavigationBar: CurvedNavigationBar(
+              height: 55,
+              backgroundColor: defaultColor,
+              index: cubit.currentIndex,
+              // ignore: prefer_const_literals_to_create_immutables
+              items: <Widget>[
+                const Icon(
+                  Icons.settings,
+                  size: 25,
+                ),
+                const Icon(
+                  Icons.home,
+                  size: 25,
+                ),
+                const Icon(
+                  Icons.person,
+                  size: 25,
+                ),
+              ],
+              onTap: (index) {
+                cubit.changeBottomNav(index);
+              },
+            ),
+          );
+        });
   }
 }
