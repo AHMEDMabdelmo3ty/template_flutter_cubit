275073eeb8c2a5d9af6cf20dde15ac73e22dde57
almurafiq <100346794+almurafiq@users.noreply.github.com>
2022-05-30 14:33:13 +0200

    اضافة فيديو

--- lib/modules/home/cubit/cubit.dart
+++ lib/modules/home/cubit/cubit.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:horizontal_card_pager/card_item.dart';
+import 'package:video_player/video_player.dart';
 
 import 'states.dart';
 
@@ -6,6 +9,32 @@ class UsersCubit extends Cubit<UsersStates> {
   UsersCubit() : super(UsersInitialState());
 
   static UsersCubit get(context) => BlocProvider.of(context);
+String url='https://whitecompressor.com/storage/files/videoplayback.mp4';
+  List<CardItem> items = [
+    IconTitleCardItem(
+      text: "Alarm",
+      iconData: Icons.access_alarms,
+    ),
+    IconTitleCardItem(
+      text: "Add",
+      iconData: Icons.add,
+    ),
+    IconTitleCardItem(
+      text: "Call",
+      iconData: Icons.add_call,
+    ),
+    IconTitleCardItem(
+      text: "WiFi",
+      iconData: Icons.wifi,
+    ),
+    IconTitleCardItem(
+      text: "File",
+      iconData: Icons.attach_file,
+    ),
+    IconTitleCardItem(
+      text: "Air Play",
+      iconData: Icons.airplay,
+    ),
+  ];
 
- 
 }
--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:horizontal_card_pager/horizontal_card_pager.dart';
 import 'package:quds_interactive/quds_translation/internals.dart';
+import '../../presentation/components/video/video.dart';
 import 'cubit/cubit.dart';
 import 'cubit/states.dart';
 
@@ -20,12 +22,27 @@ class HomeScreen extends StatelessWidget {
                 'العربية'
                 ? TextDirection.rtl
                 : TextDirection.ltr,
-            child: const SafeArea(
+            child:  SafeArea(
               child: Scaffold(
                 resizeToAvoidBottomInset: true,
                 body: SingleChildScrollView(
-                  child: Padding(
-                    padding: EdgeInsets.all(18.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(18.0),
+                        child:   Video.blocProvider(
+                          // Normally you'll get both the url and the aspect ratio from your video meta data
+                          UsersCubit.get(context).url,
+                          aspectRatio: 1.77,
+                          autoPlay: true,
+                        ),
+                      ),
+                  HorizontalCardPager(
+                    onPageChanged: (page) => print("page : $page"),
+                    onSelectedItem: (page) => print("selected : $page"),
+                    items:  UsersCubit.get(context).items,
+                  )
+                    ],
                   ),
                 ),
               ),
